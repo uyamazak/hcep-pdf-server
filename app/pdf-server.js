eb2ffbc4608c4fb2fc7602930cc7425c60422a0c
uyamazak <yu_yamazaki@bizocean.co.jp>
2018-09-25 18:56:35 +0900

    fix unhandled error handling. use unhandledRejection

--- app/pdf-server.js
+++ app/pdf-server.js
@@ -1,13 +1,13 @@
 const { hcPage } = require('./hc-page')
 const { expressApp } = require('./express-app')
+process.on('unhandledRejection', function(e){
+  console.dir(e)
+  console.error('unhandledRejection. process.exit')
+  process.exit()
+})
 
 const main = async () => {
   const browserPage = await hcPage()
   expressApp(browserPage)
 }
-try {
-  main()
-} catch(e) {
-  console.error('Unhandling error in main(). process.exit:', e)
-  process.exit()
-}
+main()
