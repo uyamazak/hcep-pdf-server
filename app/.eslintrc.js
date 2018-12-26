aec0891c862c6115161c92514009d6339da20747
uyamazak <yu_yamazaki@bizocean.co.jp>
2018-12-26 15:13:05 +0900

    add eslintrc

---
+++ app/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 8,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            2
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "never"
+        ]
+    }
+};
